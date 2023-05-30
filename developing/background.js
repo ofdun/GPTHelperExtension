b7e46a1c3621d290f49c9ced134eb44ec0ee6a12
ofdun <elik394@mail.ru>
2023-05-30 22:02:05 +0300

    store last request using chrome api

--- developing/background.js
+++ developing/background.js
@@ -8,9 +8,17 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   const selected_text = info.selectionText;
+  save_to_storage(selected_text);
   if (info.menuItemId === "askGPT") {
     chrome.tabs.create({
       url: `popup.html`,
     });
-  }
+  };
 });
+
+function save_to_storage(info) {
+  chrome.storage.sync.set(
+    {"request": JSON.stringify(info)},
+    () => { }
+  );
+}
